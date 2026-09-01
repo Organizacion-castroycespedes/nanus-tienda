a7ebf66ca25ae3aba07f9c69fa118d7aa6981c8d
IvanCastroRuizdevitech <icastror@hotmail.com>
2026-08-31 22:26:50 -0500

    chore(terminal): exclude generated deployment artifacts

--- backend-perifericos/windows-installer/assets/bundle/ManusPeripheralAgent-win-x64-0.1.0/app/shared/utils/id.util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createId = void 0;
-const node_crypto_1 = require("node:crypto");
-const createId = (prefix) => `${prefix}-${Date.now()}-${(0, node_crypto_1.randomUUID)().slice(0, 8)}`;
-exports.createId = createId;
-//# sourceMappingURL=id.util.js.map
--- backend-perifericos/windows-installer/assets/bundle/ManusPeripheralAgent-win-x64-0.1.0/start-agent.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set "AGENT_ROOT=%~dp0"
-if not defined PERIPHERALS_CONFIG_PATH set "PERIPHERALS_CONFIG_PATH=%AGENT_ROOT%config\agent.config.local.json"
-if not defined PERIPHERALS_VERSION set "PERIPHERALS_VERSION=0.1.0"
-if not exist "%LOCALAPPDATA%\Manus\PeripheralAgent\logs" mkdir "%LOCALAPPDATA%\Manus\PeripheralAgent\logs"
-if not exist "%LOCALAPPDATA%\Manus\PeripheralAgent\state" mkdir "%LOCALAPPDATA%\Manus\PeripheralAgent\state"
-"%AGENT_ROOT%runtime\node.exe" "%AGENT_ROOT%app\main.js"
